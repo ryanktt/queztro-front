61f9853169c1a34a37e1a6d08e39d44e6c9b324e
Ryan <ryankayrorv@outlook.com>
2024-06-03 13:16:20 -0900

    remove header public wip code

--- src/components/Header/HeaderPublic/HeaderPublic.tsx
+++ src/components/Header/HeaderPublic/HeaderPublic.tsx
@@ -1,77 +1,15 @@
-import {
-	HoverCard,
-	Group,
-	Button,
-	UnstyledButton,
-	Text,
-	SimpleGrid,
-	ThemeIcon,
-	Anchor,
-	Divider,
-	Center,
-	Box,
-	Burger,
-	Drawer,
-	Collapse,
-	ScrollArea,
-	rem,
-	useMantineTheme,
-} from '@mantine/core';
+import { Group, Button, Box, Burger, Drawer, ScrollArea, rem } from '@mantine/core';
 import { MantineLogo } from '@mantinex/mantine-logo';
 import { useDisclosure } from '@mantine/hooks';
-import {
-	IconNotification,
-	IconCode,
-	IconBook,
-	IconChartPie3,
-	IconFingerprint,
-	IconCoin,
-	IconChevronDown,
-} from '@tabler/icons-react';
 import AuthModal from '@components/AuthModal/AuthModal';
 import { useState } from 'react';
 import { IAuthTypes } from '@components/Auth/Auth';
 import classes from './HeaderPublic.module.scss';
 
-const mockdata = [
-	{
-		icon: IconCode,
-		title: 'Open source',
-		description: 'This Pokémon’s cry is very loud and distracting',
-	},
-	{
-		icon: IconCoin,
-		title: 'Free for everyone',
-		description: 'The fluid of Smeargle’s tail secretions changes',
-	},
-	{
-		icon: IconBook,
-		title: 'Documentation',
-		description: 'Yanma is capable of seeing 360 degrees without',
-	},
-	{
-		icon: IconFingerprint,
-		title: 'Security',
-		description: 'The shell’s rounded shape and the grooves on its.',
-	},
-	{
-		icon: IconChartPie3,
-		title: 'Analytics',
-		description: 'This Pokémon uses its flying ability to quickly chase',
-	},
-	{
-		icon: IconNotification,
-		title: 'Notifications',
-		description: 'Combusken battles with the intensely hot flames it spews',
-	},
-];
-
-export default function HeaderMegaMenu() {
+export default function HeaderPublic() {
 	const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-	const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
 	const [authOpened, { open: openAuth, close: closeAuth }] = useDisclosure(false);
 	const [authType, setAuthType] = useState<IAuthTypes>('signup');
-	const theme = useMantineTheme();
 
 	const onSignUpClick = () => {
 		setAuthType('signup');
@@ -82,85 +20,11 @@ export default function HeaderMegaMenu() {
 		openAuth();
 	};
 
-	const links = mockdata.map((item) => (
-		<UnstyledButton className={classes.subLink} key={item.title}>
-			<Group wrap="nowrap" align="flex-start">
-				<ThemeIcon size={34} variant="default" radius="md">
-					<item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-				</ThemeIcon>
-				<div>
-					<Text size="sm" fw={500}>
-						{item.title}
-					</Text>
-					<Text size="xs" c="dimmed">
-						{item.description}
-					</Text>
-				</div>
-			</Group>
-		</UnstyledButton>
-	));
-
 	return (
 		<Box pb={120}>
 			<header className={classes.header}>
 				<Group justify="space-between" h="100%">
 					<MantineLogo size={30} />
-
-					<Group h="100%" gap={0} visibleFrom="sm">
-						<a href="#" className={classes.link}>
-							Home
-						</a>
-						<HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-							<HoverCard.Target>
-								<a href="#" className={classes.link}>
-									<Center inline>
-										<Box component="span" mr={5}>
-											Features
-										</Box>
-										<IconChevronDown
-											style={{ width: rem(16), height: rem(16) }}
-											color={theme.colors.blue[6]}
-										/>
-									</Center>
-								</a>
-							</HoverCard.Target>
-
-							<HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-								<Group justify="space-between" px="md">
-									<Text fw={500}>Features</Text>
-									<Anchor href="#" fz="xs">
-										View all
-									</Anchor>
-								</Group>
-
-								<Divider my="sm" />
-
-								<SimpleGrid cols={2} spacing={0}>
-									{links}
-								</SimpleGrid>
-
-								<div className={classes.dropdownFooter}>
-									<Group justify="space-between">
-										<div>
-											<Text fw={500} fz="sm">
-												Get started
-											</Text>
-											<Text size="xs" c="dimmed">
-												Their food sources have decreased, and their numbers
-											</Text>
-										</div>
-										<Button variant="default">Get started</Button>
-									</Group>
-								</div>
-							</HoverCard.Dropdown>
-						</HoverCard>
-						<a href="#" className={classes.link}>
-							Learn
-						</a>
-						<a href="#" className={classes.link}>
-							Academy
-						</a>
-					</Group>
 
 					<Group visibleFrom="sm">
 						<Button variant="default" onClick={onLogInClick}>
@@ -178,39 +42,20 @@ export default function HeaderMegaMenu() {
 			<Drawer
 				opened={drawerOpened}
 				onClose={closeDrawer}
-				size="100%"
+				size="80%"
 				padding="md"
-				title="Navigation"
+				title="Authentication"
 				hiddenFrom="sm"
-				zIndex={1000000}
+				zIndex={200}
 			>
 				<ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-					<Divider my="sm" />
-
-					<a href="#" className={classes.link}>
-						Home
-					</a>
-					<UnstyledButton className={classes.link} onClick={toggleLinks}>
-						<Center inline>
-							<Box component="span" mr={5}>
-								Features
-							</Box>
-							<IconChevronDown style={{ width: rem(16), height: rem(16) }} color={theme.colors.blue[6]} />
-						</Center>
-					</UnstyledButton>
-					<Collapse in={linksOpened}>{links}</Collapse>
-					<a href="#" className={classes.link}>
-						Learn
-					</a>
-					<a href="#" className={classes.link}>
-						Academy
-					</a>
-
-					<Divider my="sm" />
-
 					<Group justify="center" grow pb="xl" px="md">
-						<Button variant="default">Log In</Button>
-						<Button variant="gradient">Sign Up</Button>
+						<Button variant="default" onClick={onLogInClick}>
+							Log In
+						</Button>
+						<Button variant="gradient" onClick={onSignUpClick}>
+							Sign Up
+						</Button>
 					</Group>
 				</ScrollArea>
 			</Drawer>
