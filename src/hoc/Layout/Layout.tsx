20b26d0a7805c86a3fce2ed0b336ecefafcc72d0
Ryan <ryankayrorv@outlook.com>
2024-06-03 15:47:18 -0900

    remove component duplication use

--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -1,17 +1,16 @@
-import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import './Layout.module.scss';
-import HeaderPublic from '@components/Header/HeaderPublic/HeaderPublic.tsx';
 import Footer from '@components/Footer/Footer.tsx';
 import { PropsWithChildren } from 'react';
+import HeaderPublic from '@components/Header/HeaderPublic/HeaderPublic.tsx';
 import Wrapper from './Wrapper/Wrapper.tsx';
 
 export default function Layout({ children }: PropsWithChildren) {
 	return (
-		<MantineProvider>
+		<>
 			<HeaderPublic />
 			<Wrapper>{children}</Wrapper>
 			<Footer />
-		</MantineProvider>
+		</>
 	);
 }
