fb790707e1e7b846a220f4a7f69ecda3ca6ad134
Ryan <ryankayrorv@outlook.com>
2025-01-07 05:17:32 -1000

    feat: add questionnaire form component setup

--- src/containers/Home/HomeAdmin/HomeAdmin.tsx
+++ src/containers/Home/HomeAdmin/HomeAdmin.tsx
@@ -1,13 +1,17 @@
 import '@mantine/core/styles.css';
 import Navbar from '@components/Navbar/Navbar';
-import { Container } from '@mantine/core';
+import { Box, Container } from '@mantine/core';
 import Toolbar from '@components/Toolbar/Toolbar';
+import QuestionnaireForm from '@components/QuestionnaireForm/QuestionnaireForm';
 
 export default function HomeAdmin() {
 	return (
 		<Container style={{ display: 'flex', gap: '15px' }}>
 			<Navbar />
-			<Toolbar />
+			<Box w="100%" display="flex" style={{ flexDirection: 'column', gap: '10px' }}>
+				<Toolbar />
+				<QuestionnaireForm />
+			</Box>
 		</Container>
 	);
 }
