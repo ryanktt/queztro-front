c74dd2ebc7c5bc378bda3bbb82ac9f098ef09eb7
Ryan <ryankayrorv@outlook.com>
2025-01-11 06:48:47 -1000

    feat: add mantine gradient button styles vars

--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -2,13 +2,17 @@ import '@mantine/core/styles.css';
 import Footer from '@components/Footer/Footer.tsx';
 import { PropsWithChildren } from 'react';
 import HeaderPublic from '@components/Header/HeaderPublic/HeaderPublic.tsx';
-import { MantineProvider, createTheme } from '@mantine/core';
+import { Button, MantineProvider, createTheme } from '@mantine/core';
 import AuthModal from '@components/AuthModal/AuthModal.tsx';
 import AlertStack from '@components/AlertStack/AlertStack.tsx';
+import classes from './Layout.module.scss';
 
 export default function Layout({ children }: PropsWithChildren) {
 	const theme = createTheme({
 		defaultGradient: { from: 'indigo.8', to: 'violet.6', deg: 50 },
+		components: {
+			Button: Button.extend({ classNames: classes }),
+		},
 	});
 
 	return (
