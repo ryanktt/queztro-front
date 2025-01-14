f45642b1c009942289c30a2d1e76c798c1eba896
Ryan <ryankayrorv@outlook.com>
2025-01-14 03:37:36 -1000

    refactor: rename newquestionnaire comp to upsertquestionnaire

--- src/components/Questionnaire/UpsertQuestionnaire.tsx/UpsertQuestionnaire.tsx
+++ src/components/Questionnaire/UpsertQuestionnaire.tsx/UpsertQuestionnaire.tsx
@@ -1,35 +1,22 @@
-import {
-	Box,
-	Button,
-	Center,
-	Checkbox,
-	Select,
-	Text,
-	TextInput,
-	Textarea,
-	Title,
-	useMantineTheme,
-} from '@mantine/core';
+import { Button, Center, Checkbox, Select, Text, TextInput, Textarea, Title, useMantineTheme } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { hasLength, useForm } from '@mantine/form';
-import { QuestionnaireType } from '@utils/generated/graphql';
-import { useState } from 'react';
 import DragDropList from '@components/DragDropList/DragDropList.tsx';
-import NewQuestion, { INewQuestionProps } from './NewQuestion.tsx';
-import QuestionnaireListItem, { IQuestionnaireListItemProps } from './QuestionnaireListItem.tsx';
+import { QuestionnaireType } from '@utils/generated/graphql.ts';
+import UpsertQuestion, { IQuestionProps, IUpsertQuestionProps } from '../UpsertQuestion/UpsertQuestion.tsx';
 
-export interface INewQuestionnaireProps {
+export interface IUpsertQuestionnaireProps {
 	type: QuestionnaireType | null;
 	title: string;
 	description: string;
 	requireEmail: boolean;
 	requireName: boolean;
-	questions: INewQuestionProps[];
+	questions: IQuestionProps[];
 }
 
-export default function Questionnaire() {
+export default function UpsertQuestionnaire() {
 	const theme = useMantineTheme();
-	const form = useForm<INewQuestionnaireProps>({
+	const form = useForm<IUpsertQuestionnaireProps>({
 		mode: 'controlled',
 		initialValues: {
 			type: null,
@@ -43,46 +30,57 @@ export default function Questionnaire() {
 			title: hasLength({ min: 3, max: 255 }, 'Title must be 3-255 characters long'),
 		},
 	});
-	const { type, questions } = form.getValues();
+	const { type } = form.getValues();
 
-	const [newQuestionOpen, setNewQuestionOpen] = useState(false);
-
-	const handleReorderedQuestions = (reorderedQuestions: { id: string }[]) => {
+	const handleReorderedQuestions = (reorderedQuestions: IUpsertQuestionProps[]) => {
+		const { questions } = form.getValues();
 		const updatedQuestions = reorderedQuestions
-			.map(({ id }) => questions.find((opt) => opt.id === id))
-			.filter((opt): opt is INewQuestionProps => !!opt);
+			.map(({ question }) => questions.find((q) => q.id === question?.id))
+			.filter((question): question is IQuestionProps => !!question);
 
 		form.setFieldValue('questions', updatedQuestions);
 	};
 
-	const setQuestion = (newOpt: INewQuestionProps) => {
+	const setQuestion = (newQuestion: IQuestionProps) => {
 		let foundQuestion;
-		const updatedQuestions = form.getValues().questions.map((opt) => {
-			if (opt.id === newOpt.id) {
+		const updatedQuestions = form.getValues().questions.map((question) => {
+			if (question.id === newQuestion.id) {
 				foundQuestion = true;
-				return newOpt;
+				return newQuestion;
 			}
-			return opt;
+			return question;
 		});
-
-		if (!foundQuestion) updatedQuestions.push(newOpt);
+		if (!foundQuestion) updatedQuestions.push(newQuestion);
 
 		form.setFieldValue('questions', updatedQuestions);
-		setNewQuestionOpen(false);
 	};
 
-	const questionsProps = questions.map<IQuestionnaireListItemProps>((question, i) => ({
-		id: question.id,
+	const deleteQuestion = (questionId: string) => {
+		const { questions } = form.getValues();
+		const index = questions.findIndex((question) => question.id === questionId);
+		if (index !== -1) {
+			const updatedQuestions = [...questions];
+			updatedQuestions.splice(index, 1);
+
+			form.setFieldValue('questions', updatedQuestions);
+		}
+	};
+
+	const questionsProps = form.getValues().questions.map<IUpsertQuestionProps>((question, i) => ({
+		question,
 		badge: `Question ${i + 1}`,
-		description: question.description,
+		onDelete: deleteQuestion,
+		onSet: setQuestion,
 	}));
+
 	return (
-		<Box
-			p={theme.spacing.lg}
+		<div
 			style={{
+				padding: theme.spacing.lg,
 				boxShadow: theme.shadows.sm,
+				width: '100%',
 				border: '1px solid',
-				borderColor: theme.colors.gray[3],
+				borderColor: theme.colors.gray[2],
 				borderRadius: theme.radius.lg,
 			}}
 		>
@@ -124,15 +122,13 @@ export default function Questionnaire() {
 					inputWrapperOrder={['label', 'error', 'input']}
 				/>
 				<Checkbox
-					{...form.getInputProps('requireEmail')}
-					defaultChecked={false}
+					{...form.getInputProps('requireEmail', { type: 'checkbox' })}
 					color={theme.colors.indigo[6]}
 					disabled={!type}
 					label="Require user email"
 				/>
 				<Checkbox
-					{...form.getInputProps('requireName')}
-					defaultChecked={false}
+					{...form.getInputProps('requireName', { type: 'checkbox' })}
 					color={theme.colors.indigo[6]}
 					disabled={!type}
 					label="Require user name"
@@ -145,28 +141,13 @@ export default function Questionnaire() {
 					<div>
 						<DragDropList
 							onReorder={handleReorderedQuestions}
-							itemsComponent={QuestionnaireListItem}
+							itemsComponent={UpsertQuestion}
 							itemPropsList={questionsProps}
 						/>
 					</div>
 				) : null}
-				<Button
-					color={theme.colors.indigo[7]}
-					variant="outline"
-					radius="sm"
-					w="50%"
-					display={newQuestionOpen ? 'none' : 'block'}
-					onClick={() => {
-						setNewQuestionOpen(true);
-					}}
-					size="sm"
-					disabled={!type}
-				>
-					New Question
-				</Button>
-				{newQuestionOpen ? (
-					<NewQuestion onCancel={() => setNewQuestionOpen(false)} onNewQuestion={setQuestion} />
-				) : null}
+
+				<UpsertQuestion badge="New Question" method="ADD" draggable={false} onSet={setQuestion} />
 				<Center style={{ gap: '10px' }}>
 					<Button
 						disabled={!type}
@@ -181,6 +162,6 @@ export default function Questionnaire() {
 					</Button>
 				</Center>
 			</form>
-		</Box>
+		</div>
 	);
 }
--- src/containers/Home/HomeAdmin/HomeAdmin.tsx
+++ src/containers/Home/HomeAdmin/HomeAdmin.tsx
@@ -1,16 +1,16 @@
-import '@mantine/core/styles.css';
+import UpsertQuestionnaire from '@components/Questionnaire/UpsertQuestionnaire.tsx/UpsertQuestionnaire';
+import Toolbar from '@components/Toolbar/Toolbar';
 import Navbar from '@components/Navbar/Navbar';
 import { Box, Container } from '@mantine/core';
-import Toolbar from '@components/Toolbar/Toolbar';
-import NewQuestionnaire from '@components/Questionnaire/NewQuestionnaire';
+import '@mantine/core/styles.css';
 
 export default function HomeAdmin() {
 	return (
 		<Container style={{ display: 'flex', gap: '15px' }}>
 			<Navbar />
-			<Box w="100%" display="flex" style={{ flexDirection: 'column', gap: '10px' }}>
+			<Box display="flex" style={{ flexDirection: 'column', gap: '10px' }}>
 				<Toolbar />
-				<NewQuestionnaire />
+				<UpsertQuestionnaire />
 			</Box>
 		</Container>
 	);
