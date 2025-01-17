68620d215172d5bc18d47a013d0bddeb2b8a3a51
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-01-17 14:27:47 -0300

    feat: add convertpropstogqlvars function

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -5,4 +5,18 @@ export const getGraphqlErrorCode = (error?: ApolloError | ErrorResponse): string
 	return (error?.graphQLErrors?.[0]?.extensions?.exception as { code?: string })?.code;
 };
 
-export const gqlUtils = {};
+export const convertPropsToGqlVars = <T>(props: unknown): T => {
+	if (Array.isArray(props)) {
+		return props.map(convertPropsToGqlVars).filter((item) => item !== null) as T;
+	}
+
+	if (props && typeof props === 'object') {
+		return Object.fromEntries(
+			Object.entries(props)
+				.filter(([, value]) => value !== '' && value !== null && value !== undefined)
+				.map(([key, value]) => [key, convertPropsToGqlVars(value)]),
+		) as T;
+	}
+
+	return props as T;
+};
