d815dab75552ae293c92f610441451c30285fd9f
Ryan Kayro <ryankayrorv@outlook.com>
2025-01-19 01:30:35 -0300

    refactor: add essential func comment

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -5,15 +5,16 @@ export const getGraphqlErrorCode = (error?: ApolloError | ErrorResponse): string
 	return (error?.graphQLErrors?.[0]?.extensions?.exception as { code?: string })?.code;
 };
 
+/** Removes undefined or empty string values from props obj. */
 export const convertPropsToGqlVars = <T>(props: unknown): T => {
 	if (Array.isArray(props)) {
-		return props.map(convertPropsToGqlVars).filter((item) => item !== null) as T;
+		return props.map(convertPropsToGqlVars).filter((item) => item !== undefined) as T;
 	}
 
 	if (props && typeof props === 'object') {
 		return Object.fromEntries(
 			Object.entries(props)
-				.filter(([, value]) => value !== '' && value !== null && value !== undefined)
+				.filter(([, value]) => value !== '' && value !== undefined)
 				.map(([key, value]) => [key, convertPropsToGqlVars(value)]),
 		) as T;
 	}
