da30f8de0ca61e1fe4b200efe292bd369eb56e26
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-01-19 03:34:53 -0300

    feat:  buildcreatesurveygqlcarsfromporps upsertquest aux func"

---
+++ src/components/Questionnaire/UpsertQuestionnaire.tsx/UpsertQuestionnaire.aux.ts
@@ -0,0 +1,14 @@
+import { CreateSurveyMutationVariables } from '@gened/graphql';
+import { convertPropsToGqlVars } from '@utils/graphql';
+import { buildQuestionDiscriminatorsFromProps } from '../UpsertQuestion/UpsertQuestion.aux.ts';
+import { IUpsertQuestionnaireProps } from './UpsertQuestionnaire.types.ts';
+
+export const buildCreateSurveyGqlVarsFromProps = (props: IUpsertQuestionnaireProps): CreateSurveyMutationVariables => {
+	return convertPropsToGqlVars({
+		...props,
+		questions: buildQuestionDiscriminatorsFromProps(props.questions),
+		type: undefined,
+	}) as CreateSurveyMutationVariables;
+};
+
+export const preventTE = '';
