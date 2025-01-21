5c475663da1b1dbd9653a801445085367e75df2f
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-01-20 23:25:23 -0300

    remove test code and refactor

--- src/containers/Home/HomePublic/HomePublic.tsx
+++ src/containers/Home/HomePublic/HomePublic.tsx
@@ -1,14 +1,18 @@
 import '@mantine/core/styles.css';
+import HeaderPublic from '@components/Header/HeaderPublic/HeaderPublic';
 import style from './HomePublic.module.scss';
 
 export default function HomePublic() {
 	return (
-		<div className={style.HomePublic}>
-			<h1>A fully featured questionnaire platform</h1>
-			<p>
-				Create and manage questionnaires easily - Queztro provides many form features that will cover you in any
-				situation.
-			</p>
-		</div>
+		<>
+			<HeaderPublic />
+			<div className={style.HomePublic}>
+				<h1>A fully featured questionnaire platform</h1>
+				<p>
+					Create and manage questionnaires easily - Queztro provides many form features that will cover you in
+					any situation.
+				</p>
+			</div>
+		</>
 	);
 }
