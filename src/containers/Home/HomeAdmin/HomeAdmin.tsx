ec4d8f12c9017eacbeee23018cd1c27f4b0ae02c
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-01-22 17:44:21 -0300

    add major style improvements

--- src/containers/Home/HomeAdmin/HomeAdmin.tsx
+++ src/containers/Home/HomeAdmin/HomeAdmin.tsx
@@ -1,18 +1,27 @@
 import Navbar from '@components/Navbar/Navbar';
 import Toolbar from '@components/Toolbar/Toolbar';
-import { Box, Container } from '@mantine/core';
+import { Box, Center, Container, Title } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { PropsWithChildren } from 'react';
 
 export default function HomeAdmin({ children }: PropsWithChildren) {
 	return (
-		<Container style={{ display: 'flex', gap: '15px' }}>
-			<Navbar />
-			<Box display="flex" style={{ flexDirection: 'column', gap: '10px', width: '100%' }}>
-				<Toolbar />
+		<>
+			<Center>
+				<Title m={30} size={45} c="violet.8" fw={700}>
+					Queztro
+				</Title>
+			</Center>
+			<Container style={{ display: 'flex' }}>
+				<Box pos="relative" w={0} h={0}>
+					<Navbar />
+				</Box>
+				<Box display="flex" style={{ flexDirection: 'column', gap: '20px', width: '100%' }}>
+					<Toolbar />
 
-				{children}
-			</Box>
-		</Container>
+					{children}
+				</Box>
+			</Container>
+		</>
 	);
 }
