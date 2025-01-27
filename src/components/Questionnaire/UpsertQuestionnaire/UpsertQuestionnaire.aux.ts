dd8a623d9a8cffe7f96129d4ad985a981bcfabff
Ryan <ryankayrorv@outlook.com>
2025-01-27 12:39:25 -1000

    add buildupsertquestionnaireprops aux function

--- src/components/Questionnaire/UpsertQuestionnaire/UpsertQuestionnaire.aux.ts
+++ src/components/Questionnaire/UpsertQuestionnaire/UpsertQuestionnaire.aux.ts
@@ -1,7 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import { CreateSurveyMutationVariables } from '@gened/graphql';
 import { convertPropsToGqlVars } from '@utils/graphql';
 import { buildQuestionDiscriminatorsFromProps } from './UpsertQuestion/UpsertQuestion.aux.ts';
-import { IUpsertQuestionnaireProps } from './UpsertQuestionnaire.interface.ts';
+import { EQuestionnaireType, IUpsertQuestionnaireProps, QuestionnaireTypes } from './UpsertQuestionnaire.interface.ts';
+import { QuestionTypes } from '../Questionnaire.interface.ts';
+import { IOptionProps } from './UpsertOption/UpsertOption.tsx';
 
 export const buildCreateSurveyGqlVarsFromProps = (props: IUpsertQuestionnaireProps): CreateSurveyMutationVariables => {
 	return convertPropsToGqlVars({
@@ -11,4 +14,63 @@ export const buildCreateSurveyGqlVarsFromProps = (props: IUpsertQuestionnairePro
 	}) as CreateSurveyMutationVariables;
 };
 
-export const preventTE = '';
+export const buildUpsertQuestionnaireProps = (questionnaire?: QuestionnaireTypes): IUpsertQuestionnaireProps => {
+	if (!questionnaire) {
+		return {
+			type: null,
+			title: '',
+			description: '',
+			requireEmail: false,
+			requireName: false,
+			questions: [],
+		};
+	}
+
+	return {
+		type: questionnaire.type.replace('Questionnaire', '') as EQuestionnaireType,
+		description: questionnaire.description,
+		requireEmail: questionnaire.requireEmail,
+		requireName: questionnaire.requireName,
+		title: questionnaire.title,
+		questions: questionnaire.questions.map((question: QuestionTypes) => {
+			let randomizeOptions = false;
+			let feedbackAfterSubmit = '';
+			let rightAnswerFeedback = '';
+			let wrongAnswerFeedback = '';
+			let options: IOptionProps[] = [];
+
+			if ('randomizeOptions' in question) {
+				randomizeOptions = question.randomizeOptions;
+			}
+			if ('feedbackAfterSubmit' in question) {
+				feedbackAfterSubmit = question.feedbackAfterSubmit || '';
+			}
+			if ('rightAnswerFeedback' in question) {
+				rightAnswerFeedback = question.rightAnswerFeedback || '';
+			}
+			if ('wrongAnswerFeedback' in question) {
+				wrongAnswerFeedback = question.wrongAnswerFeedback || '';
+			}
+			if ('options' in question) {
+				options = (question.options || []).map((option) => ({
+					id: option._id,
+					feedbackAfterSubmit: option.feedbackAfterSubmit || '',
+					correct: option.correct || false,
+					title: option.title,
+				}));
+			}
+			return {
+				required: question.required,
+				id: question._id,
+				feedbackAfterSubmit,
+				rightAnswerFeedback,
+				wrongAnswerFeedback,
+				randomizeOptions,
+				showCorrectAnswer: question.showCorrectAnswer,
+				description: question.description || '',
+				type: question.type,
+				options,
+			};
+		}),
+	};
+};
