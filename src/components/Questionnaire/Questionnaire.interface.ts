464ec3ff6c259ed1cbc23406ec938fc660cdc6f2
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-02-03 20:29:22 -0300

    add questionnaireform component and refactor

--- src/components/Questionnaire/Questionnaire.interface.ts
+++ src/components/Questionnaire/Questionnaire.interface.ts
@@ -1,8 +1,12 @@
 import {
 	QuestionMultipleChoice,
+	QuestionMultipleChoiceInput,
 	QuestionSingleChoice,
+	QuestionSingleChoiceInput,
 	QuestionText,
+	QuestionTextInput,
 	QuestionTrueOrFalse,
+	QuestionTrueOrFalseInput,
 	QuestionnaireExam,
 	QuestionnaireQuiz,
 	QuestionnaireSurvey,
@@ -11,3 +15,17 @@ import {
 export type QuestionnaireTypes = QuestionnaireExam | QuestionnaireSurvey | QuestionnaireQuiz;
 
 export type QuestionTypes = QuestionMultipleChoice | QuestionSingleChoice | QuestionTrueOrFalse | QuestionText;
+
+export enum EQuestionnaireType {
+	Survey = 'Survey',
+	Exam = 'Exam',
+	Quiz = 'Quiz',
+}
+
+export type IQuestionInputTypes =
+	| QuestionMultipleChoiceInput
+	| QuestionTextInput
+	| QuestionSingleChoiceInput
+	| QuestionTrueOrFalseInput;
+
+export const questionBaseInputKeys = ['showCorrectAnswer', 'description', 'required', 'weight', 'title'] as const;
---
+++ src/containers/Questionnaire/Questionnaire.aux.ts
@@ -0,0 +1,73 @@
+import { IOptionProps } from '@components/Questionnaire/OptionAccordionForm/OptionAccordionForm';
+import { IQuestionProps } from '@components/Questionnaire/QuestionAccordionForm/QuestionAccordionForm';
+import { IQuestionInputTypes, questionBaseInputKeys } from '@components/Questionnaire/Questionnaire.interface';
+import {
+	OptionInput,
+	QuestionDiscriminatorInput,
+	QuestionMultipleChoiceInput,
+	QuestionSingleChoiceInput,
+	QuestionTextInput,
+	QuestionTrueOrFalseInput,
+	QuestionType,
+} from '@gened/graphql';
+import { convertPropsToGqlVars } from '@utils/graphql';
+import { getObjSelectedFields } from '@utils/objects';
+
+export const buildGqlOptionVarsFromProps = (optionProps: IOptionProps): OptionInput => {
+	return convertPropsToGqlVars(
+		getObjSelectedFields(optionProps, ['correct', 'feedbackAfterSubmit', 'title']),
+	) as OptionInput;
+};
+
+export const buildGqlOptionsVarsFromProps = (optionsProps?: IOptionProps[]): OptionInput[] | undefined => {
+	if (!optionsProps) return undefined;
+	return optionsProps.map(buildGqlOptionVarsFromProps);
+};
+
+export const buildQuestionDiscriminatorFromProps = (questionProps: IQuestionProps): QuestionDiscriminatorInput => {
+	const question = convertPropsToGqlVars({
+		...questionProps,
+		options: buildGqlOptionsVarsFromProps(questionProps.options),
+	}) as IQuestionInputTypes;
+
+	const discriminator: QuestionDiscriminatorInput = {
+		type: questionProps.type as QuestionType,
+	};
+
+	if (question.type === QuestionType.MultipleChoice) {
+		discriminator.questionMultipleChoice = getObjSelectedFields(question as QuestionMultipleChoiceInput, [
+			...questionBaseInputKeys,
+			'rightAnswerFeedback',
+			'wrongAnswerFeedback',
+			'randomizeOptions',
+			'options',
+			'type',
+		]) as QuestionMultipleChoiceInput;
+	}
+	if (question.type === QuestionType.SingleChoice) {
+		discriminator.questionSingleChoice = getObjSelectedFields(question as QuestionSingleChoiceInput, [
+			'rightAnswerFeedback',
+			'wrongAnswerFeedback',
+			'randomizeOptions',
+			'options',
+			'type',
+		]) as QuestionSingleChoiceInput;
+	}
+	if (question.type === QuestionType.TrueOrFalse) {
+		discriminator.questionTrueOrFalse = getObjSelectedFields(question as QuestionTrueOrFalseInput, [
+			'rightAnswerFeedback',
+			'wrongAnswerFeedback',
+			'options',
+			'type',
+		]) as QuestionTrueOrFalseInput;
+	}
+	if (question.type === QuestionType.Text) {
+		discriminator.questionText = getObjSelectedFields(question as QuestionTextInput, ['type']) as QuestionTextInput;
+	}
+
+	return discriminator;
+};
+
+export const buildQuestionDiscriminatorsFromProps = (questionsProps: IQuestionProps[]) => {
+	return questionsProps.map(buildQuestionDiscriminatorFromProps);
+};
