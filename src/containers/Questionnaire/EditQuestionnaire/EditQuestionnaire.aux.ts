07ef46851cebfa45756e6242866376b89e3bd18b
Ryan <ryankayrorv@outlook.com>
2025-02-08 11:23:46 -1000

    add buildupdatesurveymutationvars function

--- src/containers/Questionnaire/EditQuestionnaire/EditQuestionnaire.aux.ts
+++ src/containers/Questionnaire/EditQuestionnaire/EditQuestionnaire.aux.ts
@@ -1,11 +1,68 @@
 /* eslint-disable no-underscore-dangle */
 import { IOptionProps } from '@components/Questionnaire/OptionAccordionForm/OptionAccordionForm';
+import { IQuestionProps } from '@components/Questionnaire/QuestionAccordionForm/QuestionAccordionForm';
 import {
 	EQuestionnaireType,
 	QuestionnaireTypes,
 	QuestionTypes,
 } from '@components/Questionnaire/Questionnaire.interface';
 import { IQuestionnaireFormProps } from '@components/Questionnaire/QuestionnaireForm/QuestionnaireForm.interface';
+import {
+	Option,
+	QuestionMethodInput,
+	QuestionMethodType,
+	QuestionnaireSurvey,
+	UpdateSurveyMutationVariables,
+} from '@gened/graphql';
+import _ from 'lodash';
+import { buildQuestionDiscriminatorFromProps } from '../Questionnaire.aux.ts';
+
+export const buildOptionsFormProps = (options: Option[]) => {
+	return options.map((option) => ({
+		id: option._id,
+		feedbackAfterSubmit: option.feedbackAfterSubmit || '',
+		correct: option.correct || false,
+		title: option.title,
+	}));
+};
+
+export const buildQuestionsFormProps = (questions: QuestionTypes[]): IQuestionProps[] => {
+	return questions.map((question: QuestionTypes) => {
+		let randomizeOptions = false;
+		let feedbackAfterSubmit = '';
+		let rightAnswerFeedback = '';
+		let wrongAnswerFeedback = '';
+		let options: IOptionProps[] = [];
+
+		if ('randomizeOptions' in question) {
+			randomizeOptions = question.randomizeOptions;
+		}
+		if ('feedbackAfterSubmit' in question) {
+			feedbackAfterSubmit = question.feedbackAfterSubmit || '';
+		}
+		if ('rightAnswerFeedback' in question) {
+			rightAnswerFeedback = question.rightAnswerFeedback || '';
+		}
+		if ('wrongAnswerFeedback' in question) {
+			wrongAnswerFeedback = question.wrongAnswerFeedback || '';
+		}
+		if ('options' in question) {
+			options = buildOptionsFormProps(question.options);
+		}
+		return {
+			required: question.required,
+			id: question._id,
+			feedbackAfterSubmit,
+			rightAnswerFeedback,
+			wrongAnswerFeedback,
+			randomizeOptions,
+			showCorrectAnswer: question.showCorrectAnswer,
+			description: question.description || '',
+			type: question.type,
+			options,
+		};
+	});
+};
 
 export const buildQuestionnaireFormProps = (questionnaire?: QuestionnaireTypes | null): IQuestionnaireFormProps => {
 	if (!questionnaire) {
@@ -24,47 +81,78 @@ export const buildQuestionnaireFormProps = (questionnaire?: QuestionnaireTypes |
 		requireEmail: questionnaire.requireEmail,
 		requireName: questionnaire.requireName,
 		title: questionnaire.title,
-		questions: questionnaire.questions.map((question: QuestionTypes) => {
-			let randomizeOptions = false;
-			let feedbackAfterSubmit = '';
-			let rightAnswerFeedback = '';
-			let wrongAnswerFeedback = '';
-			let options: IOptionProps[] = [];
-
-			if ('randomizeOptions' in question) {
-				randomizeOptions = question.randomizeOptions;
-			}
-			if ('feedbackAfterSubmit' in question) {
-				feedbackAfterSubmit = question.feedbackAfterSubmit || '';
-			}
-			if ('rightAnswerFeedback' in question) {
-				rightAnswerFeedback = question.rightAnswerFeedback || '';
-			}
-			if ('wrongAnswerFeedback' in question) {
-				wrongAnswerFeedback = question.wrongAnswerFeedback || '';
-			}
-			if ('options' in question) {
-				options = (question.options || []).map((option) => ({
-					id: option._id,
-					feedbackAfterSubmit: option.feedbackAfterSubmit || '',
-					correct: option.correct || false,
-					title: option.title,
-				}));
-			}
-			return {
-				required: question.required,
-				id: question._id,
-				feedbackAfterSubmit,
-				rightAnswerFeedback,
-				wrongAnswerFeedback,
-				randomizeOptions,
-				showCorrectAnswer: question.showCorrectAnswer,
-				description: question.description || '',
-				type: question.type,
-				options,
-			};
-		}),
+		questions: buildQuestionsFormProps(questionnaire.questions),
 	};
 };
 
-export default { buildQuestionnaireFormProps };
+const addCreateQuestionMethod = (questionProps: IQuestionProps): QuestionMethodInput => {
+	return {
+		type: QuestionMethodType.Create,
+		questionDiscriminator: buildQuestionDiscriminatorFromProps(questionProps),
+	};
+};
+
+const addDeleteQuestionMethod = (questionId: string): QuestionMethodInput => {
+	return {
+		type: QuestionMethodType.Delete,
+		questionId,
+	};
+};
+
+const addUpdateQuestionMethod = (questionProps: IQuestionProps): QuestionMethodInput => {
+	return {
+		type: QuestionMethodType.Update,
+		questionDiscriminator: buildQuestionDiscriminatorFromProps(questionProps),
+	};
+};
+
+const buildUpdateQuestionsMethods = (
+	questionsProps: IQuestionProps[],
+	questionsPropsBeforeUpdate: IQuestionProps[],
+): QuestionMethodInput[] => {
+	const questionPropsMap = new Map<string, IQuestionProps>();
+	const unhandledQuestionIdSet = new Set<string>();
+
+	questionsPropsBeforeUpdate.forEach((qst) => {
+		questionPropsMap.set(qst.id, qst);
+		unhandledQuestionIdSet.add(qst.id);
+	});
+
+	const upsertQuestionMethods = questionsProps
+		.map((question): QuestionMethodInput | null => {
+			const questionBeforeUpdate = questionPropsMap.get(question.id);
+			unhandledQuestionIdSet.delete(question.id);
+
+			if (!questionBeforeUpdate) {
+				return addCreateQuestionMethod(question);
+			}
+			if (!_.isEqual(question, questionBeforeUpdate)) {
+				return addUpdateQuestionMethod(question);
+			}
+			return null;
+		})
+		.filter((q) => q !== null) as QuestionMethodInput[];
+
+	const deleteQuestionMethods = Array.from(unhandledQuestionIdSet).map((id) => {
+		return addDeleteQuestionMethod(id);
+	});
+
+	return [...upsertQuestionMethods, ...deleteQuestionMethods];
+};
+
+export const buildUpdateSurveyMutationVariables = (
+	questionnaireProps: IQuestionnaireFormProps,
+	surveyBeforeUpdate: QuestionnaireSurvey,
+): UpdateSurveyMutationVariables => {
+	return {
+		questionnaireId: surveyBeforeUpdate._id,
+		description: questionnaireProps.description,
+		requireEmail: questionnaireProps.requireEmail,
+		requireName: questionnaireProps.requireName,
+		title: questionnaireProps.title,
+		questionMethods: buildUpdateQuestionsMethods(
+			questionnaireProps.questions,
+			buildQuestionnaireFormProps(surveyBeforeUpdate).questions,
+		),
+	};
+};
