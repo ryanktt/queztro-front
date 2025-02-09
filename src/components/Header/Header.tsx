b1a80db171891e647289c5851682942f43d3f9a1
Ryan <ryankayrorv@outlook.com>
2025-02-08 14:23:35 -1000

    add project name to header and footer

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,58 @@
+import { AuthModalContext } from '@contexts/AuthModal.context.tsx';
+import { GlobalContext } from '@contexts/Global/Global.context';
+import { Box, Burger, Button, Container, Drawer, Group, ScrollArea, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useContext } from 'react';
+import classes from './Header.module.scss';
+
+export default function Header() {
+	const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+	const authModalState = useContext(AuthModalContext).state;
+	const { isLoggedIn } = useContext(GlobalContext).state.auth;
+
+	const onSignUpClick = () => {
+		authModalState.setOpened();
+		authModalState.setType('SIGNUP');
+	};
+	const onLogInClick = () => {
+		authModalState.setOpened();
+		authModalState.setType('LOGIN');
+	};
+
+	return (
+		<Box pb={120}>
+			<header className={`${classes.header} ${isLoggedIn ? classes.loggedIn : null}`}>
+				<Container size="lg">
+					<Group justify="space-between" h="100%">
+						<p className={classes.quaestio}>Quaestio</p>
+
+						{!isLoggedIn ? (
+							<Group visibleFrom="sm">
+								<Button variant="default" onClick={onLogInClick}>
+									Log In
+								</Button>
+								<Button variant="gradient" onClick={onSignUpClick}>
+									Sign Up
+								</Button>
+							</Group>
+						) : null}
+						<Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+					</Group>
+				</Container>
+			</header>
+
+			<Drawer opened={drawerOpened} onClose={closeDrawer} size="80%" padding="md" hiddenFrom="sm" zIndex={200}>
+				<ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+					<Group justify="center" grow pb="xl" px="md">
+						<Button variant="default" onClick={onLogInClick}>
+							Log In
+						</Button>
+						<Button variant="gradient" onClick={onSignUpClick}>
+							Sign Up
+						</Button>
+					</Group>
+				</ScrollArea>
+			</Drawer>
+		</Box>
+	);
+}
--- src/containers/Home/HomeAdmin/HomeAdmin.tsx
+++ src/containers/Home/HomeAdmin/HomeAdmin.tsx
@@ -1,27 +1,20 @@
 import Navbar from '@components/Navbar/Navbar';
 import Toolbar from '@components/Toolbar/Toolbar';
-import { Box, Center, Container, Title } from '@mantine/core';
+import { Box, Container } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { PropsWithChildren } from 'react';
 
 export default function HomeAdmin({ children }: PropsWithChildren) {
 	return (
-		<>
-			<Center>
-				<Title m={30} size={45} c="violet.8" fw={700}>
-					Queztro
-				</Title>
-			</Center>
-			<Container display="flex">
-				<Box pos="relative" w={0} h={0}>
-					<Navbar />
-				</Box>
-				<Box display="flex" style={{ flexDirection: 'column', gap: '20px', width: '100%' }}>
-					<Toolbar />
+		<Container display="flex" mih={1000}>
+			<Box pos="relative" w={0} h={0}>
+				<Navbar />
+			</Box>
+			<Box display="flex" style={{ flexDirection: 'column', gap: '20px', width: '100%' }}>
+				<Toolbar />
 
-					{children}
-				</Box>
-			</Container>
-		</>
+				{children}
+			</Box>
+		</Container>
 	);
 }
