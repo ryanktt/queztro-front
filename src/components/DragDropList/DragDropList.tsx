18c65549c27f4a929a63824b153d0a1a4cf9efb8
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-02-12 00:32:28 -0300

    fix dragdroplist state update

--- src/components/DragDropList/DragDropList.tsx
+++ src/components/DragDropList/DragDropList.tsx
@@ -1,18 +1,22 @@
 import { DragDropContext, Droppable } from '@hello-pangea/dnd';
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import classes from './DragDropList.module.scss';
 
 export type IDragDrogItemProps<T> = T & { draggable?: boolean; key: string };
 
 export default function DragDropList({
-	onReorder,
 	orderedItems,
+	onReorder,
 	children,
 }: {
 	onReorder: (listState: { id: string }[]) => void;
 	orderedItems: { id: string }[];
 } & PropsWithChildren) {
-	const [state, setState] = useState(orderedItems);
+	const [state, setState] = useState<typeof orderedItems>([]);
+
+	useEffect(() => {
+		setState(orderedItems);
+	}, [orderedItems]);
 
 	const handleReorder = ({ from, to }: { from: number; to: number }) => {
 		setState(() => {
