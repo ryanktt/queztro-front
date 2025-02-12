557e1da8b8ca421268ffb39905d5648cc9d2fb69
Ryan Kayro <ryankayrorv@outlook.com>
2025-02-12 18:15:27 -0300

    remove unused hoc component

--- src/hoc/Layout/Wrapper/Wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react';
-import style from './Wrapper.module.scss';
-
-export default function Wrapper({ children }: PropsWithChildren) {
-	return <div className={style.Wrapper}>{children}</div>;
-}
