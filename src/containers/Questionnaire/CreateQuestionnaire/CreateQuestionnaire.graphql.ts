cd49c5bfce1684f7c60aba41d790d28b5545aaf8
Ryan Kayro <kairo@Ryans-MacBook-Air.local>
2025-02-11 23:35:07 -0300

    remove gql query def duplication

--- src/containers/Questionnaire/CreateQuestionnaire/CreateQuestionnaire.graphql.ts
+++ src/containers/Questionnaire/CreateQuestionnaire/CreateQuestionnaire.graphql.ts
@@ -20,26 +20,4 @@ export const CREATE_SURVEY = gql(`
 	}
 `);
 
-export const UPDATE_SURVEY = gql(`
-	mutation UpdateSurvey(
-		$questionnaireId: String!
-		$title: String
-		$questionMethods: [QuestionMethodInput!]
-		$requireEmail: Boolean
-		$requireName: Boolean
-		$description: String
-	) {
-		adminUpdateQuestionnaireSurvey(
-			questionnaireId: $questionnaireId
-			title: $title
-			questionMethods: $questionMethods
-			requireEmail: $requireEmail
-			requireName: $requireName
-			description: $description
-		) {
-			...SurveyFragment
-		}
-	}
-`);
-
 export default { CREATE_SURVEY };
