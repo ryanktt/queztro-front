72fe62d73eadfcc0a2e771ea2645225cc9e7ee29
Ryan Kayro <ryankayrorv@outlook.com>
2025-02-14 15:24:39 -0300

    add respondquestionnaire mutation defs

---
+++ src/containers/Response/Response.graphql.ts
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+export const RESPOND_QUESTIONNAIRE = gql(`
+    mutation RespondQuestionnaire(
+        $answers: [AnswerDiscriminatorInput!]!
+        $questionnaireId: String!
+        $name: String
+        $email: String
+        $startedAt: DateTime!
+        $completedAt: DateTime!
+    ) {
+        publicUpsertQuestionnaireResponse(
+            questionnaireId: $questionnaireId
+            email: $email
+            name: $name
+            completedAt: $completedAt
+            startedAt: $startedAt
+            answers: $answers
+        ) {
+            authToken
+        } 
+    }
+`);
+
+export default { RESPOND_QUESTIONNAIRE };
